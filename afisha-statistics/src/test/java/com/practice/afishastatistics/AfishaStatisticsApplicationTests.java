92a5b62c13c957d44e3c1d10ba049b7bac453685
Omirkhon <am.turgimbaev@gmail.com>
2025-07-05 01:10:50 +0500

    modules

---
+++ afisha-statistics/src/test/java/com/practice/afishastatistics/AfishaStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.afishastatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfishaStatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
